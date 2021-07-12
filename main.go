3aa61f29baf8eb6ab28658ad1fc0a70c050ba757
oonya <oonya0816@gmail.com>
2021-07-13 01:43:16 +0900

    fa

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg" : "messages",
+		})
+	})
+
+	router.GET("/m", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg" : "this is m",
+		})
+	})
+
+	router.Run(":" + port)
+}
