dc5d824d09083c32b0dba687acd574796a5714e2
oonya <oonya0816@gmail.com>
2021-07-13 02:01:29 +0900

    use postgre

--- main.go
+++ main.go
@@ -4,10 +4,49 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+
+	"database/sql"
+	_ "github.com/lib/pq"
 )
+
+func dbFunc(db *sql.DB) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error creating database table: %q", err))
+            return
+        }
+
+        if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error incrementing tick: %q", err))
+            return
+        }
+
+        rows, err := db.Query("SELECT tick FROM ticks")
+        if err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error reading ticks: %q", err))
+            return
+        }
+
+        defer rows.Close()
+        for rows.Next() {
+            var tick time.Time
+            if err := rows.Scan(&tick); err != nil {
+                c.String(http.StatusInternalServerError,
+                    fmt.Sprintf("Error scanning ticks: %q", err))
+                return
+            }
+            c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+        }
+    }
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -25,11 +64,12 @@ func main() {
 		})
 	})
 
-	router.GET("/m", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"msg" : "this is m",
-		})
-	})
+	
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatalf("Error opening database: %q", err)
+    }
 
+	router.GET("/db", dbFunc(db))
 	router.Run(":" + port)
 }
