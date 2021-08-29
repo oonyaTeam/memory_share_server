2c3ad9dd84c54311112cbc919ac5006d050e32aa
oonya <oonya0816@gmail.com>
2021-08-29 14:14:45 +0900

    ブラウザだと動かないので本番で動くか試す

--- main.go
+++ main.go
@@ -15,77 +15,80 @@ import (
 )
 
 func dbFunc(db *sql.DB) gin.HandlerFunc {
-    return func(c *gin.Context) {
-        if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-            c.String(http.StatusInternalServerError,
-                fmt.Sprintf("Error creating database table: %q\n DB:%s\n", err, os.Getenv("DATABASE_URL")))
-            return
-        }
+	return func(c *gin.Context) {
+		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error creating database table: %q\n DB:%s\n", err, os.Getenv("DATABASE_URL")))
+			return
+		}
 
-        if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-            c.String(http.StatusInternalServerError,
-                fmt.Sprintf("Error incrementing tick: %q", err))
-            return
-        }
+		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error incrementing tick: %q", err))
+			return
+		}
 
-        rows, err := db.Query("SELECT tick FROM ticks")
-        if err != nil {
-            c.String(http.StatusInternalServerError,
-                fmt.Sprintf("Error reading ticks: %q", err))
-            return
-        }
+		rows, err := db.Query("SELECT tick FROM ticks")
+		if err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error reading ticks: %q", err))
+			return
+		}
 
-        defer rows.Close()
-        for rows.Next() {
-            var tick time.Time
-            if err := rows.Scan(&tick); err != nil {
-                c.String(http.StatusInternalServerError,
-                    fmt.Sprintf("Error scanning ticks: %q", err))
-                return
-            }
-            c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-        }
-    }
+		defer rows.Close()
+		for rows.Next() {
+			var tick time.Time
+			if err := rows.Scan(&tick); err != nil {
+				c.String(http.StatusInternalServerError,
+					fmt.Sprintf("Error scanning ticks: %q", err))
+				return
+			}
+			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+		}
+	}
 }
 
-type Memory struct{
-	Memory string `json:"memory"`
+type Memory struct {
+	Memory      string    `json:"memory"`
+	Longitude   float64   `json:"longitude"`
+	Latitude    float64   `json:"latitude"`
+	Seen_author []string  `json:"seen_author"`
+	Episodes    []Episode `json:"episodes"`
+	Image       string    `json:"image"`
+	Author      string    `json:"author"`
+}
+
+type Episode struct {
+	Id        string  `json:"id"`
+	Episode   string  `json:"episode"`
 	Longitude float64 `json:"longitude"`
-	Latitude float64 `json:"latitude"`
-	Seen_author []string `json:"seen_author"`
-	Episodes []Episode `json:"episodes"`
-	Image string `json:"image"`
-	Author string `json:"author"`
+	Latitude  float64 `json:"latitude"`
 }
 
-type Episode struct{
-	Id string `json:"id"`
-	Episode string `json:"episode"`
-	Distance int `json:"distance"`
-}
-
-func connectDB() (*sql.DB, error){
+func connectDB() (*sql.DB, error) {
 	// if e := os.Getenv("DEV"); e == "DEV" {
 	// 	db, err :=  sql.Open("postgres", "user= dbname=test password= sslmode=disable host=localhost ")
-	// 	return db, err; 
+	// 	return db, err;
 	// } else {
 	// 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	// 	return db, err;
 	// }
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	return db, err;
+	return db, err
 }
 
 func main() {
 	e1 := Episode{
-		Id :"first_id",
-		Episode: "subepisode 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-		Distance : 30,
+		Id:        "first_id",
+		Episode:   "subepisode 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		Longitude: 30,
+		Latitude:  40,
 	}
 	e2 := Episode{
-		"second_id",
-		"sub episode2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-		50,
+		Id:        "second_id",
+		Episode:   "sub episode2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		Longitude: 40,
+		Latitude:  32,
 	}
 	m := Memory{
 		"main episode1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -117,40 +120,40 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"msg" : "OK",
+			"msg": "OK",
 		})
 	})
-	
+
 	router.GET("/memories", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"memories" : []Memory{m, m2},
+			"memories": []Memory{m, m2},
 		})
 	})
 
 	router.GET("/mymemories", func(c *gin.Context) {
 		// uuid := c.Query("uuid")
 		c.JSON(http.StatusOK, gin.H{
-			"memories" : []Memory{m, m2},
+			"memories": []Memory{m, m2},
 		})
 	})
 
-	router.POST("/create-memory", func(c *gin.Context)  {
+	router.POST("/create-memory", func(c *gin.Context) {
 		var mb Memory
 		if err := c.BindJSON(&mb); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
-				"err" : err.Error(),
+				"err": err.Error(),
 			})
 			return
 		}
 		c.JSON(http.StatusCreated, gin.H{
-			"msg" : "OK",
+			"msg": "OK",
 		})
 	})
 
 	db, err := connectDB()
-    if err != nil {
-        log.Fatalf("Error opening database: %q", err)
-    }
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
 
 	router.GET("/db", dbFunc(db))
 	router.Run(":" + port)
