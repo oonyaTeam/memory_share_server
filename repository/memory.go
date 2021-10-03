c96af2ad6232e7d1d0bf9e526c71b54d85215525
oonya <oonya0816@gmail.com>
2021-10-03 23:01:20 +0900

    feat: 実装したぽよ

--- repository/memory.go
+++ repository/memory.go
@@ -10,8 +10,6 @@ import (
 )
 
 func GetMemories(db *sqlx.DB) ([]model.Memory, error) {
-	log.Print("This is repository\n\n\n")
-
 	var memories []model.Memory
 	stmt := `select memories.id, memory, longitude, latitude, image, author_id, angle
 			from memories join authors on memories.author_id = authors.id`
@@ -33,46 +31,35 @@ func GetMemories(db *sqlx.DB) ([]model.Memory, error) {
 			memories[i].Episodes = episodes
 		}
 	}
-	log.Println(memories)
 	
 	return memories, nil
 }
 
-func GetMyMemories(db *sqlx.DB, uuid string) ([]model.Memory, error) {
-	e1 := model.Episode{
-		Id:        1,
-		Episode:   "subepisode 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-		Longitude: 136.597816,
-		Latitude:  34.860853,
+func GetMyMemories(db *sqlx.DB, uid string) ([]model.Memory, error) {
+	var memories []model.Memory
+	stmt := `select memories.id, memory, longitude, latitude, image, author_id, angle
+			from memories join authors on memories.author_id = authors.id
+			where uuid = $1`
+	err := db.Select(&memories, stmt, uid)
+	if err != nil {
+		return nil, err
 	}
-	e2 := model.Episode{
-		Id:        2,
-		Episode:   "sub episode2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-		Longitude: 136.599202,
-		Latitude:  34.860156,
-	}
-	m := model.Memory{
-		Memory:    "main episode1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-		Longitude: 136.601064,
-		Latitude:  34.857498,
-		Episodes:  []model.Episode{e1, e2},
-		Image:     "https://pbs.twimg.com/media/E6CYtu1VcAIjMvY?format=jpg&name=large",
-		AuthorId:  1,
-		Angle:  38.54,
-		Seen:  true,
-	}
-	m2 := model.Memory{
-		Memory:      "main episode2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-		Longitude:   136.602276,
-		Latitude:    34.856582,
-		Episodes:    []model.Episode{e1, e2},
-		Image:       "https://pbs.twimg.com/media/E6FYPWLVIAQvY04?format=jpg&name=small",
-		AuthorId:    2,
-		Angle:    67.12,
-		Seen:    false,
-	}
+	log.Println(memories)
 
-	return []model.Memory{m, m2}, nil
+	for i, memory := range memories {
+		var episodes []model.Episode
+		err = db.Select(&episodes, `select id, episode, longitude, latitude from episodes where memory_id = $1`, memory.Id)
+		if err != nil {
+			return nil, err
+		}
+		if len(episodes) == 0 {
+			memories[i].Episodes = make([]model.Episode, 0)
+		} else {
+			memories[i].Episodes = episodes
+		}
+	}
+	
+	return memories, nil
 }
 
 func CreateMemory(
