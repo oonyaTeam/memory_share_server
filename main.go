5dc8fe581b37264110e298ab1d8615063c46f812
oonya <oonya200216@gmail.com>
2022-01-07 17:24:23 +0900

    fix: 要らないdbFuncを消した

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -19,40 +18,6 @@ import (
 
 	"github.com/gin-contrib/cors"
 )
-
-func dbFunc(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q\n DB:%s\n", err, os.Getenv("DATABASE_URL")))
-			return
-		}
-
-		// if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-		// 	c.String(http.StatusInternalServerError,
-		// 		fmt.Sprintf("Error incrementing tick: %q", err))
-		// 	return
-		// }
-
-		rows, err := db.Query("SELECT tick FROM ticks")
-		if err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error reading ticks: %q", err))
-			return
-		}
-
-		defer rows.Close()
-		for rows.Next() {
-			var tick time.Time
-			if err := rows.Scan(&tick); err != nil {
-				c.String(http.StatusInternalServerError,
-					fmt.Sprintf("Error scanning ticks: %q", err))
-				return
-			}
-			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-		}
-	}
-}
 
 func connectDB() (*sqlx.DB, error) {
 	if e := os.Getenv("DEV"); e == "DEV" {
@@ -133,6 +98,5 @@ func main() {
 		authRouter.POST("/author", authorHandler.RegisterAuthor)
 	}
 
-	router.GET("/db", dbFunc(db))
 	router.Run(":" + port)
 }
